0c4b5efbf577036981b50c708b33f95d2f58c101
Tigran Petrosyan <tigr-080807@mail.ru>
2026-01-17 00:34:32 +0500

    Деплой

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,11 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
 
+import { defineConfig } from 'vite';
+
 export default defineConfig({
-  root: ".",
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-    rollupOptions: {
-      input: resolve(__dirname, "index.html"),
-    },
-  },
   server: {
     open: true,
-    port: 3000,
   },
+  base: './',
 });
